8d1dd3f39b1ed021a6ecb2d801880c3692cc4506
Dmitriy-Jakovlev <dimas39690023@gmail.com>
2020-11-30 19:45:34 +0200

    сначало проверил на null

--- js/task-3.js
+++ js/task-3.js
@@ -3,10 +3,10 @@
 const ADMIN_PASSWORD = 'jqueryismyjam';
 let message = prompt('Введите пароль');
 console.log(message);
-if (message === ADMIN_PASSWORD) {   
-    alert('Добро пожаловать!');
-} else if (message === null) {
+if (message === null) {
     alert('Отменено пользователем!');
+} else if (message === ADMIN_PASSWORD) {   
+    alert('Добро пожаловать!');
 } else {
     alert('Доступ запрещен, неверный пароль!');
 }
